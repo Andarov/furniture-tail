3703703883a0dc0932af6daac043f5559af6183e
Andarov <bekturandarob678@gmail.com>
2022-08-27 20:46:47 +0500

    Change position styles site header

--- src/main.js
+++ src/main.js
@@ -5,13 +5,13 @@ const elList = document.getElementById("list");
 elHamburger.addEventListener("click", function () {
     elHamburger.children[0].classList.toggle("hidden");
     elExit.children[0].classList.toggle("hidden");
-    elList.classList.add("right-[80px]");
+    elList.classList.add("top-[80px]");
     elList.classList.add("opacity-100");
 })
 
 elExit.addEventListener("click", function () {
     elExit.children[0].classList.toggle("hidden");
     elHamburger.children[0].classList.toggle("hidden");
-    elList.classList.remove("right-[80px]");
+    elList.classList.remove("top-[80px]");
     elList.classList.remove("opacity-100");
 })
